544dddf6133eb67b79620fcc881fab890846f747
Goutte <antoine.goutenoir@gmail.com>
2018-02-16 06:52:47 +0100

    Evade a nasty bug with ColorPicker+r8 and fix another bug wih the windows port.

--- addons/goutte.spritesheet.generator/spritesheet_generator.gd
+++ addons/goutte.spritesheet.generator/spritesheet_generator.gd
@@ -5,10 +5,21 @@ extends Node
 export(String, DIR, GLOBAL) var output_dir = ""
 # Try not to put spaces or exotic characters in here. Please? It'll melt.
 export var file_prefix = "mY_pREciOUs"
+
+# See https://github.com/godotengine/godot/issues/16736
 # We try different strategies for transparency.
-export(Color, RGB) var transparent_color_1 = Color(0, 0, 0)
+#export(Color, RGB) var transparent_color_1 = Color(0, 0, 0)
 # Useful for shadows, notably, so some strategies use two colors.
-export(Color, RGB) var transparent_color_2 = Color(0, 0, 0)
+#export(Color, RGB) var transparent_color_2 = Color(0, 0, 0)
+###
+export var bg_color_1_r = 0
+export var bg_color_1_g = 0
+export var bg_color_1_b = 0
+export var bg_color_2_r = 0
+export var bg_color_2_g = 0
+export var bg_color_2_b = 0
+###
+
 
 const DEFAULT_WINDOW_SIZE = 128
 const DEFAULT_RECORD_FRAMES = 12
@@ -27,10 +38,12 @@ func record_for_a_while(
 		duration=DEFAULT_RECORD_DURATION, frames=DEFAULT_RECORD_FRAMES,
 		window_size=Vector2(DEFAULT_WINDOW_SIZE, DEFAULT_WINDOW_SIZE)):
 	"""
+	This method is non-blocking, and will operate on _process until done.
+
 	/!. Recording will wait for two frames before actually starting.
 	    This can be compensated or overlooked, but it is annoying.
 		If you have suggestions regarding this matter, PRs are most welcome!
-	
+
 	duration: a float, in seconds. Defaults to 3.
 	frames: how many frames you want, the number of sprites in the sheet. 12.
 	window_size: Vector2 or null to disable, defaults to a 128px square.
@@ -39,6 +52,11 @@ func record_for_a_while(
 	"""
 	assert duration > 0
 	assert frames > 0
+
+	if __record_in_progress:
+		printerr("Recording is already in progress.")
+		return
+
 	__record_frames = frames
 	__record_duration = duration * 1000.0
 	__record_in_progress = true
@@ -66,7 +84,10 @@ func capture_image():
 
 func generate_spritesheet():
 	print("Generating spritesheets…")
-	
+
+	var transparent_color_1 = Color(bg_color_1_r/255.0, bg_color_1_g/255.0, bg_color_1_b/255.0)
+	var transparent_color_2 = Color(bg_color_2_r/255.0, bg_color_2_g/255.0, bg_color_2_b/255.0)
+
 	var pid
 	var is_blocking = true
 	var output = []
@@ -77,24 +98,28 @@ func generate_spritesheet():
 		transparent_color_2.r8, transparent_color_2.g8, transparent_color_2.b8
 	]
 
-	# todo: when on windows, run something like dis :
+	# When on windows, run something like dis :
 	# C:\Program Files\Git\bin>bash -c "/f/Godot\ Project\ 3.0/godot-spritesheet-generator-1.0/addons/goutte.spritesheet.generator/magic.sh "C:\\\\\\\\Users\\\\\\\\Adrenesis\\\\\\\\\Desktop\\\\\\\\" mY_pREciOUs 127 127 127 63 63 63"
 	if __is_windows():
 		var option_string = join(options, " ")
 		var space = " "
-		var quote = ""
+		var quote = "\""
 		var minus_c = "-c"
 		options = [
-		quote+file, String(quote)+__get_output_dir()+quote, file_prefix,
+		quote+file, quote+__get_output_dir()+quote, file_prefix,
 		transparent_color_1.r8, transparent_color_1.g8, transparent_color_1.b8,
 		transparent_color_2.r8, transparent_color_2.g8, transparent_color_2.b8,
 		quote
 		]
 		var options_string = join(options, space)
 		options = [ minus_c, option_string ]
-		pid = OS.execute("C:\\Program Files\\Git\\bin\\bash", options, is_blocking, output)
+		pid = OS.execute(
+			"C:\\Program Files\\Git\\bin\\bash",
+			options, is_blocking, output
+		)
 	else:
 		pid = OS.execute('/bin/bash', options, is_blocking, output)
+
 	if (not output) or (not output[0].strip_edges()):
 		printerr(
 			"ERROR: Failed to execute the bash script '%s'.\n"%file +
@@ -130,7 +155,8 @@ func _process(delta):
 	var elapsed = now - __record_started_at
 
 	if elapsed > __record_current_frame * __record_duration / __record_frames \
-	   and elapsed < __record_duration:
+	   and elapsed < __record_duration \
+	   and __record_current_frame < __record_frames:
 		capture_image()
 		__record_current_frame += 1
 
